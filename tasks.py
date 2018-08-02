8ae46cdca213efdd99b9c2fb95fd495d2e59da03
Tattoo <tatu.kairi@gmail.com>
2018-08-02 12:05:13 +0300

    run unit tests with invoke

--- tasks.py
+++ tasks.py
@@ -1,7 +1,17 @@
+from os.path import abspath, dirname, join as path_join
+
 from invoke import run, task
 
+
+CURDIR = abspath(dirname(__file__))
+SRCPATH = path_join(CURDIR, 'src')
 
 @task
 def install(context):
     run('pip install -r requirements.txt')
 
+@task
+def utest(context):
+    cmd = ('python -m unittest discover --start-directory tests/ '
+           '--top-level-directory {}'.format(CURDIR))
+    run(cmd, env={'PYTHONPATH': SRCPATH})
