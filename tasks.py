403d454d0ea69917e583bbb8cbebba0ed684e314
Tattoo <tatu.kairi@gmail.com>
2018-08-02 12:33:06 +0300

    Revert "tasks:  param was not needed after all"
    This reverts commit b49c57b6b063bb0ed119cc236cb62d710bfc80ab.

--- tasks.py
+++ tasks.py
@@ -7,16 +7,16 @@ CURDIR = abspath(dirname(__file__))
 SRCPATH = path_join(CURDIR, 'src')
 
 @task
-def install():
+def install(context):
     run('pip install -r requirements.txt')
 
 @task
-def utest():
+def utest(context):
     cmd = ('python -m unittest discover --start-directory tests/ '
            '--top-level-directory {}'.format(CURDIR))
     run(cmd, env={'PYTHONPATH': SRCPATH})
 
 @task
-def atest(rf=''):
+def atest(context, rf=''):
     run('robot --pythonpath {} --dotted {} test/'.format(SRCPATH, rf),
         pty=True)  # pty for colored output
