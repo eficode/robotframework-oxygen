84a53c3d08bb5520c69e8f11abf0e536f2e1ae17
John Lindert <john.lindert@eficode.com>
2018-08-07 11:36:36 +0300

    Exposing robot interface

--- src/oxygen/__init__.py
+++ src/oxygen/__init__.py
@@ -1,8 +1,9 @@
 from .base_handler import BaseHandler
 from .oxygen import Oxygen, OxygenLibrary
+from .robot_interface import RobotInterface
 from .version import VERSION
 
-__all__ = ['BaseHandler', 'Oxygen', 'OxygenLibrary']
+__all__ = ['BaseHandler', 'Oxygen', 'OxygenLibrary', 'RobotInterface']
 __version__ = VERSION
 
 
