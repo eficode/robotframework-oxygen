cff28368070e33c07a1893de80c9cb48f631f715
Tattoo <tatu.kairi@gmail.com>
2020-05-07 14:57:19 +0300

    OxygenLibrary: more unit tests

--- tests/utest/oxygen/test_oxygenlibrary.py
+++ tests/utest/oxygen/test_oxygenlibrary.py
@@ -1,23 +1,28 @@
 from unittest import TestCase
-from unittest.mock import patch
+from unittest.mock import patch, Mock
 
 from oxygen import BaseHandler, OxygenLibrary
+from oxygen.errors import OxygenException
 
 from ..helpers import get_config_as_file
 
 class TestOxygenLibrary(TestCase):
     EXPECTED_KEYWORDS = ('run_junit', 'run_gatling', 'run_zap')
+
+    def setUp(self):
+        self.lib = OxygenLibrary()
+
+
     def test_initialization(self):
-        lib = OxygenLibrary()
         for kw in self.EXPECTED_KEYWORDS:
-            self.assertIn(kw, lib.get_keyword_names())
+            self.assertIn(kw, self.lib.get_keyword_names())
 
     @patch('oxygen.config.CONFIG_FILE')
     def test_config_is_correct(self, mock_config):
         mock_config.return_value = get_config_as_file()
-        lib = OxygenLibrary()
-        self.assertGreater(len(lib._handlers), 1)
-        for handler in lib._handlers.values():
+
+        self.assertGreater(len(self.lib._handlers), 1)
+        for handler in self.lib._handlers.values():
             self.assertTrue(isinstance(handler, BaseHandler))
             self.assertTrue(any(hasattr(handler, kw)
                                 for kw in self.EXPECTED_KEYWORDS),
@@ -26,21 +31,37 @@ class TestOxygenLibrary(TestCase):
                                                   self.EXPECTED_KEYWORDS))
 
     def test_documentation(self):
-        lib = OxygenLibrary()
         for kw in self.EXPECTED_KEYWORDS:
-            self.assertNotNoneOrEmpty(lib.get_keyword_documentation(kw))
+            self.assertNotNoneOrEmpty(self.lib.get_keyword_documentation(kw))
         # Libdoc specific values
-        self.assertNotNoneOrEmpty(lib.get_keyword_documentation('__intro__'))
-        self.assertNotNoneOrEmpty(lib.get_keyword_documentation('__init__'))
+        self.assertNotNoneOrEmpty(self.lib.get_keyword_documentation('__intro__'))
+        self.assertNotNoneOrEmpty(self.lib.get_keyword_documentation('__init__'))
 
     def assertNotNoneOrEmpty(self, value):
-        assert (value != None or value == '',
-                'Value "{}" is None or empty'.format(value))
-
+        assert value != None or value == '', \
+               'Value "{}" is None or empty'.format(value)
 
     def test_arguments(self):
-        lib = OxygenLibrary()
         for kw in self.EXPECTED_KEYWORDS:
-            args = lib.get_keyword_arguments(kw)
+            args = self.lib.get_keyword_arguments(kw)
             self.assertIsInstance(args, list)
             self.assertGreater(len(args), 0)
+
+    @patch('oxygen.OxygenLibrary._fetch_handler')
+    def test_run_keyword(self, mock_fetch_handler):
+        expected_data = {f'{attr}.return_value': 'somefile.ext'
+                         for attr in self.EXPECTED_KEYWORDS}
+        m = Mock()
+        m.configure_mock(**expected_data)
+        mock_fetch_handler.side_effect = [m]*3
+
+        for kw in self.EXPECTED_KEYWORDS:
+            ret = self.lib.run_keyword(kw, ['somefile.ext'], {})
+            self.assertEqual(ret, 'somefile.ext')
+            self.assertNotNoneOrEmpty(self.lib.data)
+
+    def test_run_keyword_should_fail_if_nonexistent_kw_is_called(self):
+        with self.assertRaises(OxygenException) as ex:
+            self.lib.run_keyword('nonexistent', [], {})
+
+        self.assertIn('No handler for keyword', str(ex.exception))
