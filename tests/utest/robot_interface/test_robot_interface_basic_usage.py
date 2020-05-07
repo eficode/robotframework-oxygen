751397bd005da4bb23bc435fcbfc324bc7f24831
Tattoo <tatu.kairi@gmail.com>
2020-05-07 14:07:00 +0300

    RobotInterface: add unit tests

---
+++ tests/utest/robot_interface/test_robot_interface_basic_usage.py
@@ -0,0 +1,183 @@
+from time import time
+from unittest import TestCase
+
+from robot.result.model import Keyword as RobotKeyword
+from robot.result.model import Message as RobotMessage
+from robot.result.model import TestCase as RobotTest
+from robot.result.model import TestSuite as RobotSuite
+
+from oxygen import RobotInterface
+
+EXAMPLE_SUITES = [{
+  'name': 'suite1',
+  'setup': [],
+  'suites': [{'name': 'suite2',
+              'setup': {'elapsed': 0.0,
+                        'keywords': [],
+                        'messages': [],
+                        'name': 'Suite Setup keyword',
+                        'pass': True,
+                        'tags': [],
+                        'teardown': []},
+              'suites': [],
+              'tags': [],
+              'teardown': {'elapsed': 0.0,
+                           'keywords': [],
+                           'messages': [],
+                           'name': 'Suite Teardown keyword',
+                           'pass': True,
+                           'tags': [],
+                           'teardown': []},
+              'tests': [{'keywords': [{'elapsed': 0.0,
+                                       'keywords': [],
+                                       'messages': [],
+                                       'name': 'casea (Execution)',
+                                       'pass': True,
+                                       'tags': [],
+                                       'teardown': []}],
+                         'name': 'casea',
+                         'setup': {'elapsed': 0.0,
+                                   'keywords': [],
+                                   'messages': [],
+                                   'name': 'Test Setup keyword',
+                                   'pass': True,
+                                   'tags': [],
+                                   'teardown': []},
+                         'tags': ['OXYGEN_JUNIT_UNKNOWN_EXECUTION_TIME'],
+                         'teardown': {'elapsed': 0.0,
+                                      'keywords': [],
+                                      'messages': [],
+                                      'name': 'Suite Teardown keyword',
+                                      'pass': True,
+                                      'tags': [],
+                                      'teardown': []}},
+                        {'keywords': [{'elapsed': 0.0,
+                                       'keywords': [],
+                                       'messages': [],
+                                       'name': 'caseb (Execution)',
+                                       'pass': True,
+                                       'tags': [],
+                                       'teardown': []}],
+                         'name': 'caseb',
+                         'setup': [],
+                         'tags': ['OXYGEN_JUNIT_UNKNOWN_EXECUTION_TIME'],
+                         'teardown': []}]}],
+  'tags': [],
+  'teardown': [],
+  'tests': [{'keywords': [{'elapsed': 0.0,
+                           'keywords': [],
+                           'messages': [],
+                           'name': 'case1 (Execution)',
+                           'pass': True,
+                           'tags': [],
+                           'teardown': []}],
+             'name': 'case1',
+             'setup': [],
+             'tags': ['OXYGEN_JUNIT_UNKNOWN_EXECUTION_TIME'],
+             'teardown': []},
+            {'keywords': [{'elapsed': 0.0,
+                           'keywords': [],
+                           'messages': ['ERROR: Example error message '
+                                        '(the_error_type)'],
+                           'name': 'case2 (Execution)',
+                           'pass': False,
+                           'tags': [],
+                           'teardown': []}],
+             'name': 'case2',
+             'setup': [],
+             'tags': ['OXYGEN_JUNIT_UNKNOWN_EXECUTION_TIME'],
+             'teardown': []},
+            {'keywords': [{'elapsed': 0.0,
+                           'keywords': [],
+                           'messages': ['FAIL: Example failure message '
+                                        '(the_failure_type)'],
+                           'name': 'case3 (Execution)',
+                           'pass': False,
+                           'tags': [],
+                           'teardown': []}],
+             'name': 'case3',
+             'setup': [],
+             'tags': ['OXYGEN_JUNIT_UNKNOWN_EXECUTION_TIME'],
+             'teardown': []}]
+}, {
+  'name': 'suite2',
+  'setup': [],
+  'suites': [],
+  'tags': [],
+  'teardown': [],
+  'tests': [{'keywords': [{'elapsed': 0.0,
+                           'keywords': [],
+                           'messages': [],
+                           'name': 'casea (Execution)',
+                           'pass': True,
+                           'tags': [],
+                           'teardown': []}],
+             'name': 'casea',
+             'setup': [],
+             'tags': ['OXYGEN_JUNIT_UNKNOWN_EXECUTION_TIME'],
+             'teardown': []},
+            {'keywords': [{'elapsed': 0.0,
+                           'keywords': [],
+                           'messages': [],
+                           'name': 'caseb (Execution)',
+                           'pass': True,
+                           'tags': [],
+                           'teardown': []}],
+             'name': 'caseb',
+             'setup': [],
+             'tags': ['OXYGEN_JUNIT_UNKNOWN_EXECUTION_TIME'],
+             'teardown': []}]
+}]
+
+
+class  RobotInterfaceBasicTests(TestCase):
+    '''
+    This tests only two methods of RobotInterface, but since they internally use
+    all the other functions, pretty much everything is covered.
+    '''
+    def setUp(self):
+        self.interface = RobotInterface()
+
+    def test_build_suites(self):
+        now = int(round(time() * 1000))
+        timestamp, converted = self.interface.build_suites(now, *EXAMPLE_SUITES)
+
+        self.assertIsInstance(converted, list)
+        self.assertEqual(len(converted), 2)
+
+        for converted_suite in converted:
+            self.assertIsInstance(converted_suite, RobotSuite)
+
+        for subsuite in converted[0].suites:
+            self.assertIsInstance(converted_suite, RobotSuite)
+
+        for test in converted[1].tests:
+            self.assertIsInstance(test, RobotTest)
+
+        for kw in converted_suite.tests[1].keywords:
+            self.assertIsInstance(kw, RobotKeyword)
+
+        for message in converted_suite.tests[1].keywords[0].messages:
+            self.assertIsInstance(message, RobotMessage)
+
+    def test_create_wrapper_keyword_for_setup(self):
+        retval = self.interface.create_wrapper_keyword('My Wrapper',
+                                                       '20200507 13:42:50.001',
+                                                       '20200507 14:59:01.999',
+                                                       True,
+                                                       RobotKeyword(),
+                                                       RobotKeyword())
+
+        self.assertIsInstance(retval, RobotKeyword)
+        self.assertEqual(retval.name, 'My Wrapper')
+        self.assertEqual(len(retval.keywords), 2)
+        self.assertEqual(retval.type, retval.SETUP_TYPE)
+
+    def test_create_wrapper_keyword_for_setup(self):
+        retval = self.interface.create_wrapper_keyword('My Wrapper',
+                                                       '20200507 13:42:50.001',
+                                                       '20200507 14:59:01.999',
+                                                       False,
+                                                       RobotKeyword())
+
+        self.assertEqual(retval.type, retval.TEARDOWN_TYPE)
