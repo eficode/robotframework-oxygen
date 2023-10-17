3c8ad08f5a59913c8987a7b97a349ee5a211316e
Tattoo <tatu.kairi@gmail.com>
2023-10-17 15:06:26 +0300

    Change junitparser dependency, fixed to 2.0 release

--- tasks.py
+++ tasks.py
@@ -62,13 +62,14 @@ def coverage(context, in_nix=False):
           'single string. E.g: invoke atest --rf "--name my_suite"'
 })
 def atest(context, rf=''):
-    run(f'robot '
-        f'--pythonpath {str(SRCPATH)}'
+    cmd = (f'robot '
+        f'--pythonpath {str(SRCPATH)} '
         f'--dotted '
         f'{rf} '
         f'--listener oxygen.listener '
-        f'{str(CURDIR / "tests" / "atest")}',
-        pty=(not system() == 'Windows'))
+        f'{str(CURDIR / "tests" / "atest")}')
+    print(cmd)
+    run(cmd, pty=(not system() == 'Windows'))
 
 @task(help={
     'in_nix': IN_NIX_HELP
